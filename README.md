aad8c6a09c0378813de2b49f77f8ef061e1159d8
Sajjad Aemmi <sajjadaemmi@gmail.com>
2021-06-14 17:37:44 +0430

    add test

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 Shuffle Attention for MobileNetV3
 
 ![model arch](images/model_arch.png)
+
+
+## Train
+
+Run the following command for train model on your own dataset:
+```
+python train.py --dataset mnist 
+```
+
+## Train
+
+Run the following command for evaluation trained model on test dataset:
+```
+python test.py --dataset mnist
+```
+
+## Inference
+
+Run the following command for classification images:
+```
+python inference.py --input /path/to/image.jpg 
+```
+
--- test.py
+++ test.py
@@ -14,62 +14,41 @@ import dataset
 import config
 
 
-def train():
+def test():
     parser = argparse.ArgumentParser(description='SA-MobileNetV3 - Test')
     parser.add_argument('--dataset', help="dataset", default='mnist', type=str)
     parser.add_argument('--gpu', help="gpu", default=False, action='store_true')
+    parser.add_argument('--weights', help="pre trained weights path", default='./weights/mnist.pt', type=str)
     args = parser.parse_args()
 
     if args.dataset == 'mnist':
-        train_dataloader, val_dataloader, dataset_classes = dataset.mnist('test')
+        test_dataloader, dataset_classes = dataset.mnist('test')
     elif args.dataset == 'cfar100':
-        train_dataloader, val_dataloader, dataset_classes = dataset.cfar100('test')
+        test_dataloader, dataset_classes = dataset.cfar100('test')
 
     num_classes = len(dataset_classes)
 
     device = torch.device('cuda') if torch.cuda.is_available() and args.gpu else torch.device('cpu')
     model = mobilenet_v3_large(num_classes=num_classes)
     model = model.to(device)
+    model.eval()
+    model.load_state_dict(torch.load(args.weights, map_location=device), strict=False)
 
     tic = time.time()
 
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+    model.eval()
+    test_acc = 0
+    for data, labels in tqdm(test_dataloader, desc="Testing"):
+        data, labels = data.to(device), labels.to(device)
+        preds = model(data)
+        test_acc += calc_acc(preds, labels)
 
-    model.train(True)
-    for epoch in range(1, config.epochs + 1):
-        train_loss = 0.0
-        train_acc = 0.0
-        for data, labels in tqdm(train_dataloader, desc="Training"):
-            data, labels = data.to(device), labels.to(device)
-            optimizer.zero_grad()
-            preds = model(data)
-            loss = loss_fn(preds, labels)
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss
-            train_acc += calc_acc(preds, labels)
-
-        epoch_loss = train_loss / len(train_dataloader)
-        epoch_acc = train_acc / len(train_dataloader)
-
-        print(config.GREEN, f"Epoch: {epoch} [Train Loss: {epoch_loss}] [Train Accuracy: {epoch_acc}]", config.RESET)
-
-        val_acc = 0
-        for data, labels in tqdm(val_dataloader, desc="Validating"):
-            data, labels = data.to(device), labels.to(device)
-            preds = model(data)
-            val_acc += calc_acc(preds, labels)
-
-        acc = val_acc / len(val_dataloader)
-        print(config.BLUE, f"[Validation Accuracy: {acc}]", config.RESET)
-
+    acc = test_acc / len(test_dataloader)
+    print(config.BLUE, f"[Test Accuracy: {acc}]", config.RESET)
 
     tac = time.time()
     print("Time Taken : ", tac - tic)
 
-    torch.save(model.state_dict(), "mnist.pt")
 
 if __name__ == "__main__":
-    train()
+    test()
