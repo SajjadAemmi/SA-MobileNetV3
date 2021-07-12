a167aad8c7f0a765c889e23d67fac7a8b2a105f5
Sajjad Aemmi <sajjadaemmi@gmail.com>
2021-07-13 00:08:39 +0430

    typo fixed

--- dataset.py
+++ dataset.py
@@ -61,9 +61,38 @@ def cfar100(subset='train'):
         return train_dataloader, val_dataloader, train_dataset.classes
 
     elif subset == 'test':
-        test_dataset = datasets.MNIST('datasets', train=False, download=True, transform=transform)
+        test_dataset = datasets.CIFAR100('datasets', train=False, download=True, transform=transform)
 
         test_dataloader = torch.utils.data.DataLoader(test_dataset, num_workers=config.num_workers, shuffle=True, batch_size=config.batch_size)
 
         print('test', test_dataloader.dataset)
         return test_dataloader, test_dataset.classes
+
+
+def imagenet(subset='train'):
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Resize((224,224)),
+                                    transforms.RandomHorizontalFlip()
+                                    # transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    #                      std=[0.229, 0.224, 0.225]),
+                                ])
+
+    if subset == 'train':
+        train_dataset = datasets.ImageNet('datasets', split="train", download=True, transform=transform)
+        train_set, val_set = torch.utils.data.random_split(train_dataset, [40000, 10000])
+
+        train_dataloader = torch.utils.data.DataLoader(train_set, num_workers=config.num_workers, shuffle=True, batch_size=config.batch_size)
+        val_dataloader = torch.utils.data.DataLoader(val_set, num_workers=config.num_workers, shuffle=True, batch_size=config.batch_size)
+
+        print('train', train_dataloader.dataset)
+        print('val', val_dataloader.dataset)
+        return train_dataloader, val_dataloader, train_dataset.classes
+
+    elif subset == 'test':
+        test_dataset = datasets.ImageNet('datasets', split="val", download=True, transform=transform)
+
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, num_workers=config.num_workers, shuffle=True, batch_size=config.batch_size)
+
+        print('test', test_dataloader.dataset)
+        return test_dataloader, test_dataset.classes
+
